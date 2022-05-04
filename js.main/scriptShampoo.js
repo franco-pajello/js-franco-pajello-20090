31dbe6cc98500cfde698e70fbed74cb1d2e2ce78
Franco Pajello <francoagustinpajello@gmail.com>
2022-05-03 22:01:16 -0300

    agregamos alertas, local storage

--- js.main/scriptShampoo.js
+++ js.main/scriptShampoo.js
@@ -1,3 +1,13 @@
+
+window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem("carrito"));
+  if (storage.length >=1){
+    carrito=storage;
+    creamosCarrito(storage)
+  }
+}
+
+
 let carrito = [];
 const contenedor = document.getElementById("container");
 contenedor.className = "row"
@@ -27,10 +37,8 @@ fetch("./data.json")
 
         Swal.fire({
           toast: true,
-          heightAuto: false,
           padding: '1em',
           color: '#716add',
-          background: '#fff url(/images/trees.png)',
           position: 'top-end',
           icon: 'success',
           title: 'producto agregado al carrito',
@@ -43,14 +51,14 @@ fetch("./data.json")
 
         const productoRepetido = carrito.some(elemento => elemento.id === producto.id)
 
-        let prod = ""
+        let prod;
         productoRepetido ? ((prod = carrito.map(prod => {
-          ((prod.id === producto.id ? ((prod.cantidad++), (btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0)(creamosCarrito(carrito)))) : NaN))
+          ((prod.id === producto.id ? ((prod.cantidad++), (btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0),(creamosCarrito(carrito)))) : NaN))
         }))) : obtenerProducto(producto.id, producto.nombre, producto.precio, cantidad, precioTotal);
-
+           addadd()
+         
       })
     });
-
   }).catch((error) => {
     alert("error al cargar los datos")
   })
@@ -75,14 +83,14 @@ carritoModal.innerHTML =
   </div>
   <div class=" d-flex justify-content-around">
   <h5 class="d-inline"><div class="modal-footer d-inline " > 
-  TOTAL:<p id="precioTotalId" class="d-inline"> </p>
+  TOTAL: $<p id="precioTotalId" class="d-inline"> </p>
   </div></h5>
   <div class="d-inline">
   <button type="button" id="btnVaciarCarrito" class="btn btn-secondary">vaciar carrito</button>
   </div>
   </div>
   <div class="modal-footer  d-flex justify-content-between">  
-  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Comprar</button>
+  <button type="button" class="btn btn-secondary" id="btnComprar" data-bs-dismiss="modal">Comprar</button>
   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Seguir comprando</button>
   
   </div>
@@ -94,7 +102,6 @@ headerCarrito.appendChild(carritoModal);
 let btnVaciarCarrito = document.getElementById(`btnVaciarCarrito`)
 
 btnVaciarCarrito.addEventListener("click", () => {
-
 
   vaciarCarrito(carrito)
 })
@@ -124,13 +131,12 @@ function creamosCarrito(array) {
       </div> `
 
     modalCarritoBody.appendChild(productoAgregadoAlCarrito);
-
-
     let btnEliminar = document.getElementById(`btnEliminar${producto.id}`);
 
     btnEliminar.addEventListener("click", () => {
       eliminarDelCarrito(producto.id)
-
+      addadd()
+     
     })
 
   });
@@ -158,20 +164,19 @@ function armandoObjDlCarrito(productoIdd, productoNombree, productoPrecioo, prod
     total: totalPrecio
   }
   carrito.push(productoDelCarrito)
-
   creamosCarrito(carrito)
 
 }
 
 function eliminarDelCarrito(elementid) {
   const buscamosElementoId = carrito.find((producto) => producto.id === elementid)
-  console.log(carrito)
+
   const indice = carrito.indexOf(buscamosElementoId)
 
   buscamosElementoId.cantidad > 1 ? ((buscamosElementoId.cantidad--), (btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0))) : carrito.splice(indice, 1);
-
-
+  
   creamosCarrito(carrito)
+  
 }
 
 function vaciarCarrito(array) {
@@ -179,7 +184,6 @@ function vaciarCarrito(array) {
   let largoDelArrayCarritoVacio = ""
   array.length >= 1 ? ((largoDelArrayCarritoVacio = array.splice(0)), (modalCarritoBody.innerHTML = ""), (precioTotalCard.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad, 0)), (largoDelArrayCarritoVacio.push(creamosCarrito))) : (swal.fire({
     toast: true,
-    heightAuto: false,
     padding: '1em',
     color: '#716add',
     position: 'center',
@@ -189,5 +193,89 @@ function vaciarCarrito(array) {
     timer: 1000
   }))
 
+  addadd()
   btnCarrito.innerText = carrito.length = ""
+}
+let btnComprar = document.getElementById("btnComprar")
+btnComprar.addEventListener("click",()=>{
+
+
+if(carrito.length>=1){
+
+  
+  Swal.fire({
+  position: 'center',
+  icon: 'info',
+  title:"Informacion del envio",
+  html: `<form class="row g-3" id="formulario">
+  <div class="col-md-4">
+  <label for="validationDefault01" class="form-label" id="nombre">Nombre</label>
+  <input type="text" class="form-control" id="validationDefault01" value="" required>
+  </div>
+  <div class="col-md-4">
+  <label for="validationDefault02" class="form-label" id="apellido">Apellido</label>
+  <input type="text" class="form-control" id="validationDefault02" value="" required>
+  </div>
+  <div class="col-md-4">
+  <label for="validationDefaultUsername" class="form-label" id="email">Email</label>
+  <div class="input-group">
+  <input type="email" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
+  <span class="input-group-text" id="inputGroupPrepend2">@</span>
+  </div>
+  </div>
+  <div class="col-md-6">
+  <label for="validationDefault03" class="form-label">Provincia</label>
+  <input type="text" class="form-control" id="validationDefault03" required>
+  </div>
+  <div class="col-md-6">
+  <label for="validationDefault04" class="form-label">Partido</label>
+  <input type="text" class="form-control" id="validationDefault04" required>
+
+  </select>
+  </div>
+  <div class="col-md-4">
+  <label for="validationDefault05" class="form-label">C/postal</label>
+  <input type="number"  class="form-control" id="validationDefault05" required>
+  </div>
+  <div class="col-md-4">
+  <label for="validationDefault06" class="form-label">Calle</label>
+  <input type="text" class="form-control" id="validationDefault06" required>
+  </div>
+  <div class="col-md-4">
+  <label for="validationDefault07" class="form-label">Altura</label>
+  <input type="number"  min="1" class="form-control" id="validationDefault07" required>
+  </div>
+  <div class="col-12">
+  <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+  <label class="form-check-label" for="invalidCheck2">
+  Agree to terms and conditions
+  </label>
+  </div>
+  </div>
+  <div class="col-12">
+  <button class="btn btn-primary" type="submit">Confirmar compra</button >
+ 
+  </div>
+  </form>
+  <p id="log"></p>`,
+  showConfirmButton: false,
+  
+})
+
+}else{
+  Swal.fire({
+    toast:true,
+    position: 'center',
+    icon: 'error',
+    title: 'El carrito se encuentra vacio',
+    showConfirmButton: false,
+    timer: 1000
+  })
+}
+})
+
+function addadd(){
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+
 }
