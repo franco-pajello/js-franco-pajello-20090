9b5f830f28e15580b5381c67d12ca4aa8eb0ab76
Franco Pajello <francoagustinpajello@gmail.com>
2022-05-04 15:09:39 -0300

    limpiamos js

--- js.main/scriptShampoo.js
+++ js.main/scriptShampoo.js
@@ -1,16 +1,13 @@
-
-window.onload = function(){
-  const storage = JSON.parse(localStorage.getItem("carrito"));
-  if (storage.length >=1){
-    carrito=storage;
-    creamosCarrito(storage)
-  }
-}
-
-
 let carrito = [];
+
 const contenedor = document.getElementById("container");
+
 contenedor.className = "row"
+
+let headerCarrito = document.getElementById("headerCarrito");
+
+let carritoModal = document.createElement("div")
+
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
@@ -28,7 +25,6 @@ fetch("./data.json")
     <div class="btn-group" role="group" aria-label="Basic example">
     <button type="button"  class="btn btn-primary", id="btnComprar${producto.id}" idProducto="${producto.id}" >Añadir al carrito</button>
     </div> 
-    
     </div>`;
 
       contenedor.appendChild(card);
@@ -41,69 +37,62 @@ fetch("./data.json")
           color: '#716add',
           position: 'top-end',
           icon: 'success',
-          title: 'producto agregado al carrito',
+          title: 'Producto agregado al carrito',
           showConfirmButton: false,
           timer: 1000
         })
-
+        
         let cantidad = 1
+
         let precioTotal = 0
 
         const productoRepetido = carrito.some(elemento => elemento.id === producto.id)
 
         let prod;
         productoRepetido ? ((prod = carrito.map(prod => {
-          ((prod.id === producto.id ? ((prod.cantidad++), (btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0),(creamosCarrito(carrito)))) : NaN))
+          ((prod.id === producto.id ? ((prod.cantidad++), (btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0), (creamosCarrito(carrito)))) : NaN))
         }))) : obtenerProducto(producto.id, producto.nombre, producto.precio, cantidad, precioTotal);
-           addadd()
-         
+        creamosLocalStorage()
       })
     });
   }).catch((error) => {
-    alert("error al cargar los datos")
+    alert("Error al cargar los datos")
   })
-
-let headerCarrito = document.getElementById("headerCarrito");
-
-let carritoModal = document.createElement("div")
 carritoModal.innerHTML =
   `<button type="button" class="btn btn-primary badge " data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Carrito <p class="d-inline" id="btnCarrito"> </p>
-  </button>
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-  <div class="modal-content">
-  <div class="modal-header">
-  <h5 class="modal-title" id="exampleModalLabel">Carrito</h5>
-  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-  </div>
-  <div class="modal-body" id="modalCarritoBody">
-
-  
-  </div>
-  <div class=" d-flex justify-content-around">
-  <h5 class="d-inline"><div class="modal-footer d-inline " > 
-  TOTAL: $<p id="precioTotalId" class="d-inline"> </p>
-  </div></h5>
-  <div class="d-inline">
-  <button type="button" id="btnVaciarCarrito" class="btn btn-secondary">vaciar carrito</button>
-  </div>
-  </div>
-  <div class="modal-footer  d-flex justify-content-between">  
-  <button type="button" class="btn btn-secondary" id="btnComprar" data-bs-dismiss="modal">Comprar</button>
-  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Seguir comprando</button>
-  
-  </div>
-  </div>
-  </div>
-  </div>`
+Carrito <p class="d-inline" id="btnCarrito"> </p>
+</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">Carrito</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<div class="modal-body" id="modalCarritoBody">
+</div>
+<div class=" d-flex justify-content-around">
+<h5 class="d-inline"><div class="modal-footer d-inline " > 
+TOTAL: $<p id="precioTotalId" class="d-inline"> </p>
+</div></h5>
+<div class="d-inline">
+<button type="button" id="btnVaciarCarrito" class="btn btn-secondary">Vaciar carrito</button>
+</div>
+</div>
+<div class="modal-footer  d-flex justify-content-between">  
+<button type="button" class="btn btn-secondary" id="btnComprar" data-bs-dismiss="modal">Comprar</button>
+<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Seguir comprando</button>
+</div>
+</div>
+</div>
+</div>`
 headerCarrito.appendChild(carritoModal);
 
 let btnVaciarCarrito = document.getElementById(`btnVaciarCarrito`)
 
 btnVaciarCarrito.addEventListener("click", () => {
 
-  vaciarCarrito(carrito)
+  creamosLocalStorage(0)
 })
 
 let btnCarrito = document.getElementById("btnCarrito")
@@ -112,160 +101,61 @@ let precioTotalCard = document.getElementById("precioTotalId")
 
 const modalCarritoBody = document.getElementById("modalCarritoBody");
 
-function creamosCarrito(array) {
-
-  modalCarritoBody.innerHTML = ""
-
-  array.forEach(producto => {
-
-    let productoAgregadoAlCarrito = document.createElement("div")
-    productoAgregadoAlCarrito.innerHTML =
-      `<div class="alert alert-success" role="alert">
-      <h4 class="alert-heading">${producto.nombre} 
-      <hr>
-      <p class="mb-0">${producto.cantidad}</p>
-      <hr>
-      <div class=" d-flex justify-content-between">
-      <p class="mb-0 d-inline" >c/u : $${producto.precio}</p> <button id="btnEliminar${producto.id}" type="button" class="btn btn-dark d-inline">ELIMINAR</button></h4>
-      </div>
-      </div> `
-
-    modalCarritoBody.appendChild(productoAgregadoAlCarrito);
-    let btnEliminar = document.getElementById(`btnEliminar${producto.id}`);
-
-    btnEliminar.addEventListener("click", () => {
-      eliminarDelCarrito(producto.id)
-      addadd()
-     
-    })
-
-  });
-  precioTotalCard.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad, 0)
-  btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0)
-}
-
-function obtenerProducto(productoId, productoNombre, productoPrecio, cantidad, precioTotal) {
-  let productoIdd = productoId
-  let productoNombree = productoNombre
-  let productoPrecioo = productoPrecio
-  let productoCantidadd = cantidad
-  let totalPrecio = precioTotal
-  armandoObjDlCarrito(productoIdd, productoNombree, productoPrecioo, productoCantidadd, totalPrecio)
-
-}
-
-function armandoObjDlCarrito(productoIdd, productoNombree, productoPrecioo, productoCantidadd, totalPrecio) {
-
-  const productoDelCarrito = {
-    id: productoIdd,
-    nombre: productoNombree,
-    precio: productoPrecioo,
-    cantidad: productoCantidadd,
-    total: totalPrecio
-  }
-  carrito.push(productoDelCarrito)
-  creamosCarrito(carrito)
-
-}
-
-function eliminarDelCarrito(elementid) {
-  const buscamosElementoId = carrito.find((producto) => producto.id === elementid)
-
-  const indice = carrito.indexOf(buscamosElementoId)
-
-  buscamosElementoId.cantidad > 1 ? ((buscamosElementoId.cantidad--), (btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0))) : carrito.splice(indice, 1);
-  
-  creamosCarrito(carrito)
-  
-}
-
-function vaciarCarrito(array) {
-
-  let largoDelArrayCarritoVacio = ""
-  array.length >= 1 ? ((largoDelArrayCarritoVacio = array.splice(0)), (modalCarritoBody.innerHTML = ""), (precioTotalCard.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad, 0)), (largoDelArrayCarritoVacio.push(creamosCarrito))) : (swal.fire({
-    toast: true,
-    padding: '1em',
-    color: '#716add',
-    position: 'center',
-    icon: 'error',
-    title: 'El carrito se encuentra vacio',
-    showConfirmButton: false,
-    timer: 1000
-  }))
-
-  addadd()
-  btnCarrito.innerText = carrito.length = ""
-}
 let btnComprar = document.getElementById("btnComprar")
-btnComprar.addEventListener("click",()=>{
+btnComprar.addEventListener("click", () => {
+  if (carrito.length >= 1) {
 
-
-if(carrito.length>=1){
-
-  
+    Swal.fire({
+      position: 'center',
+      icon: 'info',
+      title: "Información del envío",
+      html: `<form class="row g-3" id="formulario">
+      <div class="col-md-4">
+      <label for="validationDefault01" class="form-label" id="nombre">Nombre</label>
+      <input type="text" class="form-control" id="validationDefault01" value="" required>
+      </div>
+      <div class="col-md-4">
+      <label for="validationDefault02" class="form-label" id="apellido">Apellido</label>
+      <input type="text" class="form-control" id="validationDefault02" value="" required>
+      </div>
+      <div class="col-md-4">
+      <label for="validationDefaultUsername" class="form-label" id="email">Email</label>
+      <div class="input-group">
+      <input type="email" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
+      <span class="input-group-text" id="inputGroupPrepend2">@</span>
+      </div>
+      </div>
+      <div class="col-md-6">
+      <label for="validationDefault03" class="form-label" id="provincia">Provincia</label>
+      <input type="text" class="form-control" id="validationDefault03" required>
+      </div>
+      <div class="col-md-6">
+      <label for="validationDefault04" class="form-label" id="localidad">Localidad</label>
+      <input type="text" class="form-control" id="validationDefault04" required>
+      </select>
+      </div>
+      <div class="col-md-4">
+      <label for="validationDefault05" class="form-label" id="postal">Cod. postal</label>
+    <input type="number" min="0"  class="form-control" id="validationDefault05" required>
+    </div>
+    <div class="col-md-4">
+    <label for="validationDefault06" class="form-label " id="calle">Calle</label>
+    <input type="text" class="form-control" id="validationDefault06" required>
+    </div>
+    <div class="col-md-4">
+    <label for="validationDefault07" class="form-label" id="numero">Número</label>
+    <input type="number"  min="0" class="form-control" id="validationDefault07" required>
+    </div>
+    <div class="col-md-12">
+    <button type="submit"  class="btn btn-info text-white"><h5><spam>Confirmar compra</spam></h5></button>
+    </div>
+    </form>`,
+      showConfirmButton: false,
+      
+    })
+} else {
   Swal.fire({
-  position: 'center',
-  icon: 'info',
-  title:"Informacion del envio",
-  html: `<form class="row g-3" id="formulario">
-  <div class="col-md-4">
-  <label for="validationDefault01" class="form-label" id="nombre">Nombre</label>
-  <input type="text" class="form-control" id="validationDefault01" value="" required>
-  </div>
-  <div class="col-md-4">
-  <label for="validationDefault02" class="form-label" id="apellido">Apellido</label>
-  <input type="text" class="form-control" id="validationDefault02" value="" required>
-  </div>
-  <div class="col-md-4">
-  <label for="validationDefaultUsername" class="form-label" id="email">Email</label>
-  <div class="input-group">
-  <input type="email" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
-  <span class="input-group-text" id="inputGroupPrepend2">@</span>
-  </div>
-  </div>
-  <div class="col-md-6">
-  <label for="validationDefault03" class="form-label">Provincia</label>
-  <input type="text" class="form-control" id="validationDefault03" required>
-  </div>
-  <div class="col-md-6">
-  <label for="validationDefault04" class="form-label">Partido</label>
-  <input type="text" class="form-control" id="validationDefault04" required>
-
-  </select>
-  </div>
-  <div class="col-md-4">
-  <label for="validationDefault05" class="form-label">C/postal</label>
-  <input type="number"  class="form-control" id="validationDefault05" required>
-  </div>
-  <div class="col-md-4">
-  <label for="validationDefault06" class="form-label">Calle</label>
-  <input type="text" class="form-control" id="validationDefault06" required>
-  </div>
-  <div class="col-md-4">
-  <label for="validationDefault07" class="form-label">Altura</label>
-  <input type="number"  min="1" class="form-control" id="validationDefault07" required>
-  </div>
-  <div class="col-12">
-  <div class="form-check">
-  <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-  <label class="form-check-label" for="invalidCheck2">
-  Agree to terms and conditions
-  </label>
-  </div>
-  </div>
-  <div class="col-12">
-  <button class="btn btn-primary" type="submit">Confirmar compra</button >
- 
-  </div>
-  </form>
-  <p id="log"></p>`,
-  showConfirmButton: false,
-  
-})
-
-}else{
-  Swal.fire({
-    toast:true,
+    toast: true,
     position: 'center',
     icon: 'error',
     title: 'El carrito se encuentra vacio',
@@ -275,7 +165,96 @@ if(carrito.length>=1){
 }
 })
 
-function addadd(){
-  localStorage.setItem("carrito", JSON.stringify(carrito))
+/* funciones */
 
+function creamosCarrito(array) {
+  
+  modalCarritoBody.innerHTML = ""
+  
+  array.forEach(producto => {
+    
+    let productoAgregadoAlCarrito = document.createElement("div")
+    productoAgregadoAlCarrito.innerHTML =
+    `<div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">${producto.nombre} 
+    <hr>
+    <p class="mb-0">${producto.cantidad}</p>
+    <hr>
+    <div class=" d-flex justify-content-between">
+    <p class="mb-0 d-inline" >c/u : $${producto.precio}</p> <button id="btnEliminar${producto.id}" type="button" class="btn btn-dark d-inline">ELIMINAR</button></h4>
+    </div>
+    </div> `
+    
+    modalCarritoBody.appendChild(productoAgregadoAlCarrito);
+    let btnEliminar = document.getElementById(`btnEliminar${producto.id}`);
+    
+    btnEliminar.addEventListener("click", () => {
+      eliminarDelCarrito(producto.id)
+      creamosLocalStorage()
+    })
+    
+  });
+  precioTotalCard.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad, 0)
+  btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0)
 }
+
+  function obtenerProducto(productoId, productoNombre, productoPrecio, cantidad, precioTotal) {
+    let productoIdd = productoId
+    let productoNombree = productoNombre
+    let productoPrecioo = productoPrecio
+    let productoCantidadd = cantidad
+    let totalPrecio = precioTotal
+    armandoObjDlCarrito(productoIdd, productoNombree, productoPrecioo, productoCantidadd, totalPrecio)
+  }
+  
+  function armandoObjDlCarrito(productoIdd, productoNombree, productoPrecioo, productoCantidadd, totalPrecio) {
+    
+    const productoDelCarrito = {
+      id: productoIdd,
+      nombre: productoNombree,
+      precio: productoPrecioo,
+      cantidad: productoCantidadd,
+      total: totalPrecio
+    }
+    carrito.push(productoDelCarrito)
+    creamosCarrito(carrito)
+  }
+  
+  function eliminarDelCarrito(elementid) {
+    const buscamosElementoId = carrito.find((producto) => producto.id === elementid)
+    
+    const indice = carrito.indexOf(buscamosElementoId)
+  
+    buscamosElementoId.cantidad > 1 ? ((buscamosElementoId.cantidad--), (btnCarrito.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0))) : carrito.splice(indice, 1);
+    
+    creamosCarrito(carrito)
+  }
+  
+  function vaciarCarrito(array) {
+    let largoDelArrayCarritoVacio = ""
+    array.length >= 1 ? ((largoDelArrayCarritoVacio = array.splice(0)), (modalCarritoBody.innerHTML = ""), (precioTotalCard.innerText = carrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad, 0)), (largoDelArrayCarritoVacio.push(creamosCarrito))) : (swal.fire({
+      toast: true,
+      padding: '1em',
+      color: '#716add',
+      position: 'center',
+      icon: 'error',
+      title: 'El carrito se encuentra vacío',
+      showConfirmButton: false,
+      timer: 1000
+    }))
+    creamosLocalStorage()
+    btnCarrito.innerText = carrito.length = ""
+  }
+  
+  function creamosLocalStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+window.onload = function () {
+  const storage = JSON.parse(localStorage.getItem("carrito"));
+  storage ? ((carrito = storage),(creamosCarrito(storage))) : ""
+}
+
+
+
+
+
